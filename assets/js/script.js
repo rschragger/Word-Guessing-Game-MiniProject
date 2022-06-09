dcc16fe11d260c04baebc50657ad6a657e618489
rschragger <reveschragger@gmail.com>
2022-06-09 21:31:19 +1000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//Create variables ---------------------------------------------------------
+
+// Types of character to use
+// Note: variable names deliberately "obscured" to prevent using reserved words. Also, I want to control which symbols are used to deter ones which might cause issues
+var uCaseLett = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+// HTML items
+var startBtn = document.querySelector("#start-btn");
+var resetScoreBtn = document.querySelector("#reset-btn");
+
+
+//Create Functions ---------------------------------------------------------
+
+//Initialisation
+function init(){
+}
+
+//What happens when the start button is pressed
+function StartButton(){}
+
+function
+
+
+
+
+Init()
