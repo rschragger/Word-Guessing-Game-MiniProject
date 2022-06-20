fb0c1450e49b94d0f6aaf141a55d727be52b62d2
rschragger <80379457+rschragger@users.noreply.github.com>
2022-06-20 22:27:04 +1000

    Update formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 //Project management table
+
+//jquery (in case)
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
 //Create variables ---------------------------------------------------------
 
-
+//projectName  projectType  hourlyRate dueDate daysUntilDueDate potentialTotalEarnings deleteRow
 
 
 // HTML items
